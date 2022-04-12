1a11d31291f0567b0d7d7a54873b2a7a35eaea21
clare <clarepritchard@hotmail.com>
2022-04-12 16:43:58 +0100

    Java examples so that we didn't have the solutions

--- main.js
+++ main.js
@@ -79,3 +79,79 @@ switch(theDay){
     console.log(`Excuse me?`);
     break;
 }
+
+// testing whether we now about random in javascript - nothing to do with logic
+function selectANumber(){
+    let myNum = Math.random();
+    return myNum;
+}
+// generate two integers below 50 randomly
+function blackJack(){
+    firstCard = Math.floor(Math.random() * 50);
+    secondCard = Math.floor(Math.random() * 50);
+    console.log(firstCard + ", " + secondCard);
+    firstResult = 21 - firstCard;
+    secondResult = 21 - secondCard;
+    if(firstResult <0 && secondResult <0){
+        console.log(0);
+    } 
+    else if(firstResult < 0 ){
+        console.log(secondCard);
+    }
+    else if(secondResult <0){
+        console.log(firstCard);
+    }
+    else if(firstResult > secondResult){
+        console.log(secondCard);
+    }
+    else{
+        console.log(firstCard);
+    }
+}
+blackJack();
+
+//add three integers if they are different, ignore duplicates, use each different number once
+function adder(num1, num2, num3){
+    let satArray = [num1, num2, num3];
+    let satSet = new Set(satArray);
+    let total = 0;
+    satSet.forEach(function(value){
+        total = total + value;
+    } )
+    console.log(total);  
+
+}
+adder(2, 5, 5);
+
+//add all different numbers
+function adder2(num1, num2, num3){
+    let myArray = [num1, num2, num3];
+    if(num1=== num2 && num1 === num3){
+        console.log(0);
+        
+    }
+    else if(num1===num2){
+        console.log(num3);
+    }
+    else if(num1 === num3){
+        console.log(num2);
+    }
+    else if (num2 === num3){
+        console.log(num1);
+    }
+    else{
+        let total = 0
+        for(let i =0; i<myArray.length; i++){
+            total = total + myArray[i];
+        }
+        console.log(total);
+    }
+}
+
+adder2(8,4,6);
+// don't forget arrays can be sorted
+function adder3(myArray){
+    let myArray = myArray;
+    let mySortedArray = myArray.sort();
+    
+}
