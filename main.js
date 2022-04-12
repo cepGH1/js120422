3e427ea0723cfbb8c3dd37773f235946cbcc18bc
clare <clarepritchard@hotmail.com>
2022-04-12 14:21:18 +0100

    iteration exercises

--- main.js
+++ main.js
@@ -1,49 +1,81 @@
-console.log("Hello World");
+console.log("helo world");
 
-var myFirstVar = "hello world";
-
-//ECMA 6 var not used so much more specific scope
-
-//var is hoisted
-
-//use these
-let mySecondvar = "can be modified";
-const neverChangingValue ="a constant";
-
-//numerical variables are all Number
-// command typeof
-let myNumber = 2.3;
-console.log(myNumber);
-//arrays
-let myArray = [1, 5, "hello"]; //indexing starts at zero
-//arrays are mutable and can contain a mixture of data types
-//make extra spaces in an array
-myArray[50] = "51st place";
-console.log(myArray[50]);
-//now has 51 components
-
-//types
-//number, string, null, undefined, array, object
-//concatenation of strings + makes the thing that follows also a string
-let str = "10 + 10" + 10;
-console.log(str);
-//be careful about doing math in your concatenation
-let str2 = "10+10" + 10 + 10;
-console.log(str2);
-let str3 = `this is a literal using the variable ${myArray[2]}` ;
-console.log(str3);
-//use that type of camel - default for javascript and java
-
-//using css in the console command put a  comma and then the css as text
-let myTxt = "This could be useful!";
-
-console.log("this is a %c a message with some CSS."+str3,"color: yellow; font-style: italic; background-color: blue;padding: 2px");
-
-console.log( "%c" + str3,"color:red; font-style:bold;");
-console.log("This is now %c the end of the exercises.","color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
-
-function myFunction(){
-    console.log(`hello world`);
-
+let myArray = [1,2,3,4,5,6,7,8,9];
+let total = 0;
+//for loop
+for(let i=0; i<myArray.length; i++){
+    console.log(myArray[i]);
+    total += myArray[i];
 }
-myFunction();
+console.log(total);
+
+//while loop
+let total2 = 0;
+let message = 1;
+while(total2 < 4){
+
+   console.log("hello" + message);
+   message ++;
+    total2 = total2 + 1;
+}
+
+//exercise 1
+let A = 100;
+while(A < 201){
+    console.log(""+ A);
+    A++
+}
+
+//Exercise 2
+let a = 100;
+while (a <= 200) {
+  if (a % 2 == 0) {
+    console.log("even number");
+  } else {
+    console.log("odd number");
+  }
+  a++;
+}
+
+// exercise 3
+//nested for loops
+for (let i = 0; i < 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+      console.log(i);
+    }
+ }  
+
+ for (let a = 100; a <= 200; a++) {
+    console.log(`a = ${a}`);
+  }
+// a for loop for the modulus exercise
+  for (let a = 100; a <= 200; a++) {
+    if (a % 2 == 0) {
+      console.log("even number");
+    } else {
+      console.log("odd number");
+    }
+  }
+
+let today = new Date();
+let theDay = today.getDay();
+
+switch(theDay){
+    case 0:
+        console.log(`It's Sunday`);
+    break;
+    case 6:
+        console.log(`It's Saturday`);
+    break;
+    case 1:
+    console.log(`It's Monday`);
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+    console.log(`It's a weekday`);
+    break;
+  default:
+    console.log(`Excuse me?`);
+    break;
+}
