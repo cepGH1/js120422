e6b41da8868b2a6939a437c8c61ff1a4dd6ac298
clare <clarepritchard@hotmail.com>
2022-04-13 11:49:09 +0100

    stringify example added

--- myFirstJSON.js
+++ myFirstJSON.js
@@ -1,4 +1,4 @@
-`use strict`
+'use strict'
 console.log("hello world");
 // jason object 1
 let plato = {
@@ -20,3 +20,47 @@ console.log(`Darth Vader is a Jedi? ${darthVader.sith ? false : true}`);
 //add to json objects with . notation
 plato.furtherKey = "new value";
 console.log(plato);
+
+let daveCar ={
+    make:"ford",
+    model: "T",
+    colour:"black",
+    name: "chunk"
+};
+
+let clareCar ={
+    make:"volkswagen",
+    model: "polo",
+    colour:"blue",
+    name: "toffee"
+};
+//an array of json objects, can add other objects manually
+let garage = [daveCar, clareCar, {name:"liver", model:"mondeo", make:"ford", colour:"red"}];
+
+//enhanced for loop
+for(let car in garage){
+    console.log(garage[car].make);
+};
+
+//nested array
+let massiveJSON = {
+    smallerJSON:{
+        "students":[{
+            "clare": "liverpool",
+            "seer": "burnley",
+            "fred": "warrington"
+        },
+    {chancer:"villian", dodger:"rishi"}],
+    "heros":["angel", "superman", "leon"]
+    }
+};
+
+//stringify retrives data from JSON and changes it to a string
+console.log(JSON.stringify(plato));
+let myString = JSON.stringify(plato);
+console.log(myString);
+
+console.log(JSON.stringify(massiveJSON.smallerJSON.students));
+
+//JSON objects don't allow single quotes
+
